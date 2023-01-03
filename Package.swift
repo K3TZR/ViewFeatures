18d5662f4abb092a5ac8f23e7b9ba89d7ffa5d83
Douglas Adams <douglas.adams@me.com>
2023-01-03 11:11:59 -0500

    v2.2.2

--- Package.swift
+++ Package.swift
@@ -31,8 +31,8 @@ let package = Package(
     .library(name: "TxFeature", targets: ["TxFeature"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/K3TZR/ApiFeatures.git", from: "1.5.1"),
-    .package(url: "https://github.com/K3TZR/SharedFeatures.git", from: "1.3.1"),
+    .package(url: "https://github.com/K3TZR/ApiFeatures.git", from: "1.11.1"),
+    .package(url: "https://github.com/K3TZR/SharedFeatures.git", from: "1.6.1"),
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.42.0"),
   ],
   targets: [
